aa9892ce9e43a2e3c3dd4bc8db1011e253ccef49
Ben Tagtow <bentagtow@gmail.com>
2019-11-08 21:45:23 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -6,8 +6,7 @@ BASE_HOA = {
 
 
 def add_character(show, name)
-  show[name]=new_character
-  new_character.unshift
+  show[name]
   # Write your implementation here
   # Should return the array of the 'show' argument
 end
